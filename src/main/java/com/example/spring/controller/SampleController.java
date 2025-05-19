b08fdb0d406b4b40fa092082434c001ee47a1e11
yeonji123 <76023703+yeonji123@users.noreply.github.com>
2025-05-19 09:37:11 +0900

    feat: controller에서 hello 출력하기

---
+++ src/main/java/com/example/spring/controller/SampleController.java
@@ -0,0 +1,12 @@
+package com.example.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SampleController {
+    @GetMapping("/hello")
+    public String[] hello(){
+        return new String[]{"Hello","World"};
+    }
+}
