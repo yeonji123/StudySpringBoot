e3b732cc4f10df7b70fdb2273fd596d7560c27b7
yeonji123 <76023703+yeonji123@users.noreply.github.com>
2025-05-19 15:22:02 +0900

    feat: dto를 생성 후 thymeleaf로 반복문 출력하기

--- src/main/java/com/example/spring/controller/SampleController.java
+++ src/main/java/com/example/spring/controller/SampleController.java
@@ -1,11 +1,18 @@
 package com.example.spring.controller;
 
+import com.example.spring.dto.SampleDTO;
 import lombok.extern.log4j.Log4j2;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 @Log4j2
@@ -19,4 +26,21 @@ public class SampleController {
     public void ex1(){
         log.info("ex1.........");
     }
+
+    @GetMapping({"/sample/ex2"})
+    public void exModel(Model model){
+        List<SampleDTO> list = IntStream.rangeClosed(1,20).asLongStream(). mapToObj(i -> {
+            SampleDTO dto = SampleDTO.builder()
+                    .sno(i)
+                    .first("First..."+i)
+                    .last("Last..."+i)
+                    .regTime(LocalDateTime.now())
+                    .build();
+            return dto;
+        }).collect(Collectors.toList());
+
+        model.addAttribute("list", list);
+
+    }
+
 }
