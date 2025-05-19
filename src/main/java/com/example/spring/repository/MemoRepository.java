2a6e8491069efce042f036da7d703cf5e79b079e
yeonji123 <76023703+yeonji123@users.noreply.github.com>
2025-05-19 14:15:20 +0900

    feat: @Query를 사용해서 get, update 진행하기(param, #, ? 등 사용)

--- src/main/java/com/example/spring/repository/MemoRepository.java
+++ src/main/java/com/example/spring/repository/MemoRepository.java
@@ -1,13 +1,29 @@
 package com.example.spring.repository;
 
 import com.example.spring.entity.Memo;
+import jakarta.transaction.Transactional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface MemoRepository extends JpaRepository<Memo, Long>{
     List<Memo> findByMnoBetweenOrderByMnoDesc(Long from, Long to);
     Page<Memo> findByMnoBetween(Long from, Long to, Pageable pageable);
     void deleteMemoByMnoLessThan(Long num);
+
+    @Query("select m from Memo m order by m.mno desc")
+    List<Memo> getListDesc();
+    // ?1, ?2, :xxx, .#{} 을 사용해서 쿼리에 where문 대신 사용가능
+    @Transactional
+    @Modifying
+//    @Query("update Memo m set m.memoText = :memoText where m.mno = :mno ")
+    @Query("update Memo m set m.memoText = :#{#param.memoText} where m.mno = :#{#param.mno}")
+    int updateMemoText(@Param("mno") Long mno, @Param("memoText") String memoText);
+
+
 }
