b62f1158f88d9da707c24619d49a2e5859dbe935
yeonji123 <76023703+yeonji123@users.noreply.github.com>
2025-05-25 19:55:11 +0900

    feat: service 내에서 inser 코드 함수 생성

--- src/main/java/com/example/spring/service/MemberService.java
+++ src/main/java/com/example/spring/service/MemberService.java
@@ -1,4 +1,16 @@
 package com.example.spring.service;
 
+import com.example.spring.entity.Member;
+import com.example.spring.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public Member save(){
+
+    }
 }
