891b77e9d380e4e97c9b232cd55043a861b16c47
yeonji123 <76023703+yeonji123@users.noreply.github.com>
2025-05-25 19:51:16 +0900

    feat: Member entity 생성

--- src/main/java/com/example/spring/entity/Member.java
+++ src/main/java/com/example/spring/entity/Member.java
@@ -1,4 +1,26 @@
 package com.example.spring.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Value;
+
+@Entity
+@Getter
+@Setter
+@Table(name="tbl_member", catalog = "user")
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+    private String number;
+    private String gender;
+
+    @Column(nullable = true)
+    private String address;
+
+
 }
