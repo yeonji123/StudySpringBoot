ef1dec8dcc2d312854553ed33d56f126caa57b58
yeonji123 <76023703+yeonji123@users.noreply.github.com>
2025-05-25 19:51:28 +0900

    feat: repository 초기 생성

--- src/main/java/com/example/spring/repository/MemberRepository.java
+++ src/main/java/com/example/spring/repository/MemberRepository.java
@@ -1,4 +1,9 @@
 package com.example.spring.repository;
 
-public class MemberRepository {
+import com.example.spring.entity.Member;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
 }
